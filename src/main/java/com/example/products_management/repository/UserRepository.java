41f97ee74eecb9736e5e9244e6a0e6ba455f0e7a
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-09-28 22:42:58 +0700

    feat(repository): Add UserRepository, ProductRepository, and OrderRepository

--- src/main/java/com/example/products_management/repository/UserRepository.java
+++ src/main/java/com/example/products_management/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.products_management.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.products_management.entity.User;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
     boolean existsByUsername(String userName);
+    Optional<User> findByUsername(String username);
 }  
