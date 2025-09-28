bd6568918ca88480a6c7fde637f311952dbfb4a1
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-09-28 22:30:54 +0700

    Feat: Implement global exception handling

--- src/main/java/com/example/products_management/exception/ErrorCode.java
+++ src/main/java/com/example/products_management/exception/ErrorCode.java
@@ -1,26 +1,32 @@
 package com.example.products_management.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public enum ErrorCode {
-    USER_EXISTED(1001, "User existed."),
-    INVALID_KEY(1001, "Invalid message key."),
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error."),
-    USERNAME_INVALID(1003, "Username must be at least 3 characters."),
-    PASSWORD_INVALID(1004, "Username must be at least 8 characters."),
+    USER_EXISTED(1001, "User existed.", HttpStatus.BAD_REQUEST),
+    INVALID_KEY(1002, "Invalid message key.", HttpStatus.BAD_REQUEST),
+    UNCATEGORIZED_EXCEPTION(1007, "Uncategorized error.", HttpStatus.INTERNAL_SERVER_ERROR),
+    USERNAME_INVALID(1003, "Username must be at least 3 characters.", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004, "Username must be at least 8 characters.", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1005, "User not existed.", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "You don't have permission", HttpStatus.FORBIDDEN)
     ;
     
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
-    private int code;
-    private String message;
-
-    public int getCode() {
-        return code;
-    }
-    public String getMessage() {
-        return message;
-    }
-
-    
+    int code;
+    String message;
+    HttpStatusCode statusCode;  
 }
